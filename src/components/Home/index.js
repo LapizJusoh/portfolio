2075eb38fd1355a0c420ccb5b274e0c365a57219
Hafizuddin <lapizjusoh@yahoo.com>
2022-11-14 08:16:57 +0800

    Change to REACT.

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <h1>This is the Home</h1>
+    </nav>
+  )
+}
+
+export default Navigation;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <h1>This is the navigation</h1>
+    </nav>
+  )
+}
+
+export default Navigation;
